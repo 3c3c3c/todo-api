e991c083a6482efddb9c91329584ad45eeb072e9
crispy <pembrokechris@gmail.com>
2016-06-23 09:16:24 +0000

    refactored api using underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3090;    //port variable for heroku or the local one of 3090 whichever is available
 var todos = [];
@@ -19,12 +20,12 @@ app.get('/todos', function(req, res){
 // GET /todos/:id  - get one 
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function(todo){
-        if(todoId === todo.id){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id:todoId});
+    // todos.forEach(function(todo){
+    //     if(todoId === todo.id){
+    //         matchedTodo = todo;
+    //     }
+    // });
     
    if(matchedTodo) {
        res.json(matchedTodo);
@@ -35,9 +36,21 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-    var body = req.body;
+    //use underscore to only use set field properties and not pass any others also to filter out unwanted additions of properties
+    //var body = req.body;
+   var body = _.pick(req.body, 'description', 'completed');
+    
+    //validate input if any of the fields 'completed', 'description', are empty then return a 404 not found error status
+    //check if completed exists as a boolean or description exists as a string and is not an empty string.
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(404).send();
+    }
+     
+    //remove any unwanted spaces from start and end of description field
+    body.description = body.description.trim();
+    
     body.id = todoNextId++;
-    //todoNextId++;
+    
     todos.push(body);
     res.json(body);
     console.log(body);
