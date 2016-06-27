1529849c12375f92116577180cf68f53148a14cd
crispy <pembrokechris@gmail.com>
2016-06-27 12:18:48 +0000

    added put http to update todos

--- server.js
+++ server.js
@@ -66,6 +66,45 @@ app.delete('/todos/:id', function(req, res){
     } else {
         return res.status(404).json({"error":"Not deleted as no item found to delete"});
     }
+}); 
+
+// PUT /todos/:id  -- equates to update in the crud
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);  //get the id of the item to update from the given object
+    var matchedTodo = _.findWhere(todos, {id:todoId}); //place that object in temporary variable
+    //get the object item sent to be updated on the required fields
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    //if no item sent over for update then exit before running any more code
+    if(!matchedTodo) {
+        return res.status(404).send();  //404 - not found
+    }
+    
+    //verify completed field
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')){
+        return res.status(400).send();  //400 - bad request
+    } else {
+        
+    }
+    
+    //verify description field
+    if( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')){
+        return res.status(400).send(); //400 - bad request
+    } else {
+        
+    }
+    
+    //if got to here then can do the update
+    //due to javasecipt passing objects by ref and not by value (would not work for primitives non objects)
+    //the update is covered by this line where the original is updated with the new and returned all automatically
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo); //response json automatically sends back a success status 200 
+    
 });
 
 app.listen(PORT, function(){
