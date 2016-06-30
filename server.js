173be8647c87d95dbd52e462abfb80ffbc8c1ae4
crispy <pembrokechris@gmail.com>
2016-06-30 20:25:01 +0000

    addes query to get totos

--- server.js
+++ server.js
@@ -12,10 +12,21 @@ app.get('/', function(req, res){
     res.send('Todo API Root');  
 });
 
-//  GET /todos -get all
+//  GET /todos -get all plus if add query string get all completed true or false
 app.get('/todos', function(req, res){
-    res.json(todos);
-});
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
+        filteredTodos = _.where(filteredTodos, {completed:true});
+    } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false' ) {
+        filteredTodos = _.where(filteredTodos, {completed:false});
+    } 
+         
+    res.json(filteredTodos);
+    
+}); 
+
+
 
 // GET /todos/:id  - get one 
 app.get('/todos/:id', function(req, res){
