9333fb616ee7196fc1adfdc6417c2c233ba640df
crispy <pembrokechris@gmail.com>
2016-07-01 11:24:54 +0000

    final non database version

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res){
     res.send('Todo API Root');  
 });
 
-//  GET /todos -get all plus if add query string get all completed true or false
+//  GET /todos -
 app.get('/todos', function(req, res){
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -21,6 +21,12 @@ app.get('/todos', function(req, res){
     } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false' ) {
         filteredTodos = _.where(filteredTodos, {completed:false});
     } 
+    
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ) {
+        filteredTodos = _.filter(filteredTodos, function(todo){
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+    }
          
     res.json(filteredTodos);
     
