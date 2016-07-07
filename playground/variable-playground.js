37b958725527ec74538a4420a58c213899f2b693
Chris Pembroke <pembrokechris@gmail.com>
2016-07-07 18:28:49 +0100

    Added sequelize & sqlite demo file

---
+++ playground/variable-playground.js
@@ -0,0 +1,11 @@
+var arr = ['apple','pear','bananna','avocado'];
+
+function changeArr(a[]) {
+    a = ['BEE','pear','bananna','avocado'];
+    return a;
+}
+
+
+//console.log(arr); //original
+
+console.log(changeArr(a));
